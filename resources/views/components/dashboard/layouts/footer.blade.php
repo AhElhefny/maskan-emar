860b343bfb95da0998dc341b136e88eda4e7b11c
Ahmed Elhefny <aelhefny@badee.com.sa>
2023-03-27 14:11:04 +0200

    copy right dashboard

--- resources/views/components/dashboard/layouts/footer.blade.php
+++ resources/views/components/dashboard/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2023 Nebras Films, Developed With Love By <a class="text-bold-800 grey darken-2" href="https://badee.com.sa/" target="_blank">Badee</a></span>
+    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2023 Maskan Emar, Developed With Love By <a class="text-bold-800 grey darken-2" href="https://badee.com.sa/" target="_blank">Badee</a></span>
         <span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i class="feather icon-heart pink"></i></span>
         <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
     </p>
